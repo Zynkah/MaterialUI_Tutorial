05faee20a2c8e2e37d6f9a516bd7851983946f28
Zynkah <ffgurl07@yahoo.com>
2023-02-10 11:07:44 -0800

    Theme

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,63 +4,91 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
 import { AccessTime } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          class="img"
-          src="https://th.bing.com/th/id/OIP.Tth3GWRfkrd1Ah3t7-wfwQHaE9?pid=ImgDet&rs=1"
-          alt="niagra falls"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            class="img"
+            src="https://th.bing.com/th/id/OIP.Tth3GWRfkrd1Ah3t7-wfwQHaE9?pid=ImgDet&rs=1"
+            alt="niagra falls"
+          />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Immerse into the Falls
+            </Typography>
+            <Box
               sx={{
-                width: 12.5,
+                display: "flex",
+                alignItems: "center",
               }}
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              <small>5 hours</small>
-            </Typography>
+            >
+              <AccessTime
+                sx={{
+                  width: 12.5,
+                }}
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              marginTop={3}
+            >
+              <Rating
+                name="read-only"
+                value={4.5}
+                precision={0.5}
+                size="small"
+                readOnly
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C $147
+              </Typography>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-            marginTop={3}
-          >
-            <Rating
-              name="read-only"
-              value={4.5}
-              precision={0.5}
-              size="small"
-              readOnly
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={1.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From C $147
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
